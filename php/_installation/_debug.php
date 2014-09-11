3a20c59bd74f9c2efb90700b6fd558e3b81c75e1
Graehu (Graham Hughes) <Graehu@gmail.com>
2014-09-11 22:54:32 +0100

    Added a New Database Code
    New php to install new bookmark database.

--- php/_installation/_debug.php
+++ php/_installation/_debug.php
@@ -4,13 +4,13 @@
  * This is a helper file that simply outputs the content of the users.db file.
  * Might be useful for your development.
  */
-
+// USERS SECTION
 // error reporting config
 error_reporting(E_ALL);
 
 // config
 $db_type = "sqlite";
-$db_sqlite_path = "../users.db";
+$db_sqlite_path = "../../users.db";
 
 // create new database connection
 $db_connection = new PDO($db_type . ':' . $db_sqlite_path);
@@ -23,4 +23,83 @@ $query = $db_connection->prepare($sql);
 $query->execute();
 
 // show all the data from the "users" table inside the database
+var_dump($query->fetchAll());
+
+///BOOKMARKS SECTION
+
+$db_type = "sqlite";
+$db_sqlite_path = "../../bookmarks.db";
+
+// create new database connection
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+
+// query
+$sql = 'SELECT * FROM bookmarks';
+
+// execute query
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+// show all the data from the "bookmarks" table inside the database
+var_dump($query->fetchAll());
+
+///TAG SECTION
+
+$db_type = "sqlite";
+$db_sqlite_path = "../../bookmarks.db";
+
+// create new database connection
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+
+// query
+$sql = 'SELECT * FROM tags';
+
+// execute query
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+// show all the data from the "tags" table inside the database
+var_dump($query->fetchAll());
+
+
+///TAG SECTION
+
+$db_type = "sqlite";
+$db_sqlite_path = "../../bookmarks.db";
+
+// create new database connection
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+
+// query
+$sql = 'SELECT * FROM bookmark_tags';
+
+// execute query
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+// show all the data from the "bookmark_tags" table inside the database
+var_dump($query->fetchAll());
+
+///TAG SECTION
+
+$db_type = "sqlite";
+$db_sqlite_path = "../../bookmarks.db";
+
+// create new database connection
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+
+// query
+
+
+$sql = 'SELECT bookmarks.bookmark_id, tags.tag_id
+        FROM bookmarks
+        INNER JOIN bookmark_tags
+        ON bookmark_tags.bookmark_id = bookmarks.bookmark_id
+        INNER JOIN tags
+        ON tags.tag_id = bookmark_tags.tag_id';
+
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+// show all the data from the "joined" table inside the database
 var_dump($query->fetchAll());
---
+++ php/_installation/_installbookmarks.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This is the installation file for the 0-one-file version of the php-login script.
+ * It simply creates a new and empty database.
+ */
+
+// error reporting config
+error_reporting(E_ALL);
+
+// config
+$db_type = "sqlite";
+$db_sqlite_path = "../../bookmarks.db";
+
+// create new database file / connection (the file will be automatically created the first time a connection is made up)
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+
+// create new empty table inside the database (if table does not already exist)
+$sql = 'CREATE TABLE IF NOT EXISTS `bookmarks` (
+        `bookmark_id` INTEGER PRIMARY KEY,
+        `user_id` INTEGER,
+        `bookmark_title` varchar(256),
+        `bookmark_url` varchar(256),
+        `bookmark_date` varchar(64)
+        );
+        CREATE UNIQUE INDEX `user_id_UNIQUE` ON `bookmarks` (`user_id` ASC);
+        CREATE UNIQUE INDEX `bookmark_id_UNIQUE` ON `bookmarks` (`bookmark_id` ASC);
+        ';
+
+// execute the above query
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+$sql = 'CREATE TABLE IF NOT EXISTS `tags` (
+        `tag_id` INTEGER PRIMARY KEY,
+       `tag` VARCHAR(16)
+       )';
+$query = $db_connection->prepare($sql);
+$query->execute();
+//Create joined table bookmark_tags
+$sql = 'CREATE TABLE IF NOT EXISTS `bookmark_tags`(
+       `bookmark_id` INTEGER NOT NULL,
+       `tag_id` INTEGER NOT NULL,
+       PRIMARY KEY(`bookmark_id`, `tag_id`)
+       )';
+$query = $db_connection->prepare($sql);
+$query->execute();
+
+// check for success
+if (file_exists($db_sqlite_path)) {
+    echo "Database $db_sqlite_path was created, installation was successful.";
+} else {
+    echo "Database $db_sqlite_path was not created, installation was NOT successful. Missing folder write rights ?";
+}
