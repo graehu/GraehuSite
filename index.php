285912a0e5f4d72c3030b1f7ff72a53f8ac268d2
Graehu (Graham Hughes) <Graehu@gmail.com>
2014-09-20 19:18:42 +0100

    Making Favicon Work
    Hopefully.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="images/code-edit-teal.png" rel="shortcut icon">
+    <link href="images/favicon.png" rel="shortcut icon">
 
     <title>Graehu</title>
 
