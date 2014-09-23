1c1074e3a84d27301b366258b09c2e49b451a5db
Graehu (Graham Hughes) <Graehu@gmail.com>
2014-09-23 12:19:42 +0100

    Moving The Favicon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="images/favicon.png" rel="shortcut icon">
+    <link href="favicon.png" rel="shortcut icon">
 
     <title>Graehu</title>
 
