a228d297a2b8ea280e2b15038168010304411f6f
Graham Hughes <graehu@gmail.com>
2015-03-10 20:12:27 +0000

    Better Markdown CSS
    Well I say better, but hopefully it's better.
    I'm not 100% sure about some of the values, but it's easier to read for me.
    
    hopefully it's not horrible looking!!

--- partials/markdown.tpl.php
+++ partials/markdown.tpl.php
@@ -1,9 +1,17 @@
+<div class="markdown-header">
+  The Typings
+  <div class="graehu-logo"></div>
+</div>
+<div class="markdown-subtitle">*clickty* *clack*</div>
 <div class="modal-body">
 <link rel="stylesheet" type="text/css" href="bower_components/highlight/styles/solarized_dark.css">
   <a class="close" ng-click="closePartial()"></a>
-  <div style="max-width: 1024px; width:90%; margin: 0 auto; border: 1px #333 solid;">
+  <div class="markdown-body">
     <div style="width:90%; margin: 2em auto;">
      <?php echo passthru ('markdown ' . '../'.$_GET["src"]);?>
    </div>
   </div>
+</div>
+<div class="markdown-footer">
+  The Typings © Graham Hughes 2015
 </div>
